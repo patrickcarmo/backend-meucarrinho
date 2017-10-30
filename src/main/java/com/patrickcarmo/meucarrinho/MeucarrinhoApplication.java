03587a3863f13c7ef6086628fd47e9200a272395
Patrick Carmo <patrick.a.carmo@gmail.com>
2017-10-30 20:33:41 -0200

    first commit

---
+++ src/main/java/com/patrickcarmo/meucarrinho/MeucarrinhoApplication.java
@@ -0,0 +1,12 @@
+package com.patrickcarmo.meucarrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucarrinhoApplication.class, args);
+	}
+}
