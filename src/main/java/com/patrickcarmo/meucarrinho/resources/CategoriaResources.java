93c90f7ecef161fc8b7a130058c613aabfcc38e4
Patrick Carmo <patrick.a.carmo@gmail.com>
2017-10-30 22:12:48 -0200

    Repository e Service para Categoria

--- src/main/java/com/patrickcarmo/meucarrinho/resources/CategoriaResources.java
+++ src/main/java/com/patrickcarmo/meucarrinho/resources/CategoriaResources.java
@@ -3,15 +3,22 @@ package com.patrickcarmo.meucarrinho.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patrickcarmo.meucarrinho.domain.Categoria;
+import com.patrickcarmo.meucarrinho.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
+	
+	@Autowired
+	private CategoriaService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
@@ -24,6 +31,12 @@ public class CategoriaResources {
 		lista.add(cat2);
 		
 		return lista;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/patrickcarmo/meucarrinho/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.patrickcarmo.meucarrinho.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.patrickcarmo.meucarrinho.domain.Categoria;
+import com.patrickcarmo.meucarrinho.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+	
+}
