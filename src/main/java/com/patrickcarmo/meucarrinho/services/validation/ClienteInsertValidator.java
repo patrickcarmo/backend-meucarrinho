3c168b32c1a4b41b91fc8aab567fab8986872779
Patrick Carmo <patrick.a.carmo@gmail.com>
2017-12-01 19:02:36 -0200

    Validação de email

--- src/main/java/com/patrickcarmo/meucarrinho/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/patrickcarmo/meucarrinho/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.patrickcarmo.meucarrinho.domain.Cliente;
 import com.patrickcarmo.meucarrinho.domain.enums.TipoCliente;
 import com.patrickcarmo.meucarrinho.dto.ClienteNewDTO;
+import com.patrickcarmo.meucarrinho.repositories.ClienteRepository;
 import com.patrickcarmo.meucarrinho.resources.exception.FieldMessage;
 import com.patrickcarmo.meucarrinho.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,7 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
