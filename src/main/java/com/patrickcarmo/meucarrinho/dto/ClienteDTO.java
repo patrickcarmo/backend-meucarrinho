de50e6dd9c5375fbf61ded8a64c9618d4eb93c17
Patrick Carmo <patrick.a.carmo@gmail.com>
2017-12-01 19:22:28 -0200

    Validação de email Update

--- src/main/java/com/patrickcarmo/meucarrinho/dto/ClienteDTO.java
+++ src/main/java/com/patrickcarmo/meucarrinho/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.patrickcarmo.meucarrinho.domain.Cliente;
+import com.patrickcarmo.meucarrinho.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
