7f6584207f42eb0a4fa573eb8f7f632b451924a9
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-18 20:56:21 -0200

    senha cliente

--- src/main/java/com/patrickcarmo/meucarrinho/dto/ClienteNewDTO.java
+++ src/main/java/com/patrickcarmo/meucarrinho/dto/ClienteNewDTO.java
@@ -26,6 +26,9 @@ public class ClienteNewDTO implements Serializable{
 	private Integer tipo;
 	
 	@NotEmpty(message="Preenchimento obrigatorio")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
 	private String logradouro;
 	
 	@NotEmpty(message="Preenchimento obrigatorio")
@@ -147,6 +150,14 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 }
--- src/main/java/com/patrickcarmo/meucarrinho/services/DBService.java
+++ src/main/java/com/patrickcarmo/meucarrinho/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.patrickcarmo.meucarrinho.domain.Categoria;
@@ -32,6 +33,9 @@ import com.patrickcarmo.meucarrinho.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -117,7 +121,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "patrick.alvesdc@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "patrick.alvesdc@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
