7b4af0ee915455f9f717a85da5aa49f95b9b5fcc
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-23 23:07:50 -0200

    restricao, cliente recupera seus pedidos

--- src/main/java/com/patrickcarmo/meucarrinho/services/PedidoService.java
+++ src/main/java/com/patrickcarmo/meucarrinho/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.patrickcarmo.meucarrinho.domain.Cliente;
 import com.patrickcarmo.meucarrinho.domain.ItemPedido;
 import com.patrickcarmo.meucarrinho.domain.PagamentoComBoleto;
 import com.patrickcarmo.meucarrinho.domain.Pedido;
@@ -15,6 +19,8 @@ import com.patrickcarmo.meucarrinho.repositories.ItemPedidoRepository;
 import com.patrickcarmo.meucarrinho.repositories.PagamentoRepository;
 import com.patrickcarmo.meucarrinho.repositories.PedidoRepository;
 import com.patrickcarmo.meucarrinho.repositories.ProdutoRepository;
+import com.patrickcarmo.meucarrinho.security.UserSS;
+import com.patrickcarmo.meucarrinho.services.exceptions.AuthorizationException;
 import com.patrickcarmo.meucarrinho.services.exceptions.ObjectNotFoundException;
 
 
@@ -77,6 +83,16 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
