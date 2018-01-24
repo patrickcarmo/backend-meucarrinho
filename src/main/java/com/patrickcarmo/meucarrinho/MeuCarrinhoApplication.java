cf7c79a8fcf331117def95e3b7b1771a82f67496
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-24 21:45:09 -0200

    salvando arquivo no S3

--- src/main/java/com/patrickcarmo/meucarrinho/MeuCarrinhoApplication.java
+++ src/main/java/com/patrickcarmo/meucarrinho/MeuCarrinhoApplication.java
@@ -1,11 +1,17 @@
 package com.patrickcarmo.meucarrinho;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.patrickcarmo.meucarrinho.services.S3Service;
+
 @SpringBootApplication
 public class MeuCarrinhoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuCarrinhoApplication.class, args);
@@ -13,7 +19,7 @@ public class MeuCarrinhoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {	
-		
+		s3Service.uploadFile("C:\\temp\\fotos\\Gg.png");
 	}
 	
 }
