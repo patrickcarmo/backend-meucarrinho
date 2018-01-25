e0caa5dbd78a769847325ae375b92ccb295a50f6
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-24 22:27:06 -0200

    enviando imagem via endpoint

--- src/main/java/com/patrickcarmo/meucarrinho/MeuCarrinhoApplication.java
+++ src/main/java/com/patrickcarmo/meucarrinho/MeuCarrinhoApplication.java
@@ -1,17 +1,11 @@
 package com.patrickcarmo.meucarrinho;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.patrickcarmo.meucarrinho.services.S3Service;
-
 @SpringBootApplication
 public class MeuCarrinhoApplication implements CommandLineRunner{
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuCarrinhoApplication.class, args);
@@ -19,7 +13,7 @@ public class MeuCarrinhoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {	
-		s3Service.uploadFile("C:\\temp\\fotos\\Gg.png");
+		
 	}
 	
 }
