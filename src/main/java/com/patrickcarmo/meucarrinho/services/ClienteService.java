11723eaac26155e3d667206e568a59ee7a26192e
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-01-24 22:58:28 -0200

    salvando URL da imagem em cliente

--- src/main/java/com/patrickcarmo/meucarrinho/services/ClienteService.java
+++ src/main/java/com/patrickcarmo/meucarrinho/services/ClienteService.java
@@ -115,7 +115,18 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		URI uri = s3Service.uploadFile(multipartFile);
+		Cliente cliente = repo.findOne(user.getId());
+		cliente.setImageUrl(uri.toString());
+		repo.save(cliente);
+		
+		return uri;
 	}
 	
 }
